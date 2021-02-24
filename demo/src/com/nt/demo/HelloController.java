6ecfefd3c9c2b19d2a6a642ef77f1b5bb158650e
team <team@gmail.com>
2021-02-23 21:45:07 -0800

    devvvvv

--- demo/src/com/nt/demo/HelloController.java
+++ demo/src/com/nt/demo/HelloController.java
@@ -8,5 +8,14 @@ public class HelloController {
 
 		System.out.println("Good Morning");
 	}
+	
+	public void hi() {
+		System.out.println("Hello");
+	}
+
+	
+	public void wish() {
+		System.out.println("HELLLLLLo");
+	}
 
 }
